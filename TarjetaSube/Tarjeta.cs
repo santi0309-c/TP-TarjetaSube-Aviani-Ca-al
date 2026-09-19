d08d897a8d2b031e24ec9cc305f9b15b166f8273
Valentino Aviani <valentinoaviani@gmail.com>
2026-09-19 10:46:16 -0300

    feat: implementar clase Tarjeta con validaciones y recargas

--- TarjetaSube/Tarjeta.cs
+++ TarjetaSube/Tarjeta.cs
@@ -8,6 +8,48 @@ public class Tarjeta
     {
         2000m, 3000m, 4000m, 5000m, 8000m, 10000m, 15000m, 20000m, 25000m, 30000m
     };
-
     
+    public int Id { get; set;}
+
+    public decimal Saldo {get; private set; }
+
+    public Tarjeta(decimal saldoInicial = 0)
+    {
+        if (saldoInicial < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(saldoInicial), "El saldo inicial no puede ser negativo.");
+        }
+        if (saldoInicial > LimiteSaldo)
+        {
+            throw new ArgumentOutOfRangeException(nameof(saldoInicial), "El saldo inicial supera el límite máximo de $40.000 pesos.");
+        }
+
+        Saldo = saldoInicial;
+    }
+
+    public bool CargarSaldo(decimal monto)
+    {
+        if (!CargasAceptadas.Contains(monto))
+        {
+            return false;
+        }
+
+        if (Saldo + monto > LimiteSaldo)
+        {
+            return false;
+        }
+        Saldo += monto;
+        return true;
+    }
+
+    public bool DescontarSaldo (decimal monto)
+    {
+        if (monto <= 0 || Saldo < monto)
+        {
+            return false;
+        }
+
+        Saldo -= monto;
+        return true;
+    }
 }
